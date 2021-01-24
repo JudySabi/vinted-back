f7b2bdabffbecb9706a1bde12add34611935a449
Julie Mahe <j-mahe@hotmail.fr>
2021-01-24 11:55:31 +0100

    change cloudinary name

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config(process.env.CLOUDINARY_CONFIG);
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_PUBLIC_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
 
 app.use(formidable());
 app.use(cors());
 
 // BDD vinted
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
 // ROUTES
 const userRoutes = require("./routes/user");
